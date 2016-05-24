b25ab5a6727b9323e21cbd9e70977da405b875b1
BJIAST <bjiast.01@gmail.com>
2016-05-24 17:02:38 +0300

    Added price-filtr+timer

--- double.user.js
+++ double.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CSGODouble withdraw script
 // @namespace    http://tampermonkey.net/
-// @version      0.0001
+// @version      0.0002
 // @description  try to take over the world!
 // @author       BJIAST
 // @match        http://www.csgodouble.com/withdraw.php
@@ -9,14 +9,92 @@
 // ==/UserScript==
 (function () {
     'use strict';
+    var name = $(".dropdown a b").html();
 
 
-    function hello() {
+    function htmlwriter() {
 
-        var name = $(".dropdown a b").html();
         var hellomes = "Привет " + name + ", работаем?";
         $(".alert-danger b").text(hellomes);
 
+        // alert
+        $(".btn-group").append("<label class='price-filter'>");
+
+
+        $(".price-filter").append("<div class='form-filter'>from <input type='text' id='filterCoinsFrom' class='form-control'> to <input type='text' id='filterCoinsTo' class='form-control'><ul><li><a id='filterBtn' class='btn btn-warning'>Отфильтровать</a></li><li><a id='resetFilter' class='btn btn-danger'>Сбросить</a></li></ul>");
+
+
+        // sort inputs
+    }
+
+    function styles() {
+
+        $(".price-filter").css({
+            "padding": "20px 0",
+            "overflow": "hidden",
+            "width": "100%"
+        });
+
+        $(".price-filter .form-filter").css({
+            "width": "200px",
+            "margin": "0 auto"
+        });
+        $(".form-filter ul").css({
+            "display" : "inline-block",
+            "list-style-type" : "none"
+        });
+        $(".form-filter ul li").css({
+            "float" : "left;"
+        });
+        $(".form-filter ul li a").css({
+            "width" : "120px"
+            
+        }); 
+    }
+
+    function filterByCoins() {
+        console['log']('Filter');
+        var _0x7eb2x11 = parseInt($('#filterCoinsFrom')['val']()),
+            _0x7eb2x12 = parseInt($('#filterCoinsTo')['val']());
+        $('#left>.bricks')['remove']();
+        $('#left>.reals>.placeholder')['attr']('data-price', function (_0x7eb2x13, _0x7eb2x14) {
+            _0x7eb2x14 < _0x7eb2x11 || _0x7eb2x14 > _0x7eb2x12 ? $(this)['css']({
+                display: 'none'
+            }) : $(this)['css']({
+                display: 'inline-block'
+            })
+        })
+    }
+
+
+    function timetoreload() {
+        var countdown = $('.alert-danger b'),
+            timer;
+
+        function startCountdown() {
+            var startFrom = 150;
+
+            timer = setInterval(function () {
+                countdown.text("Перегружать базу можно через " + --startFrom + " секунд");
+                if (startFrom <= 0) {
+                    clearInterval(timer);
+                    if (confirm('Перегрузить страницу:')) {
+                        setInterval(function () {
+                            location.reload();
+                        }, 1000);
+                    }
+                }
+            }, 1000);
+        }
+        startCountdown();
+    }
+
+    function resetFilter() {
+        $('#filterCoinsFrom')['val']('');
+        $('#filterCoinsTo')['val']('');
+        $('#left>.reals>.placeholder')['css']({
+            display: 'inline-block'
+        })
     }
 
     function DoAfter() {
@@ -27,13 +105,18 @@
                 data: "price",
                 order: "asc"
             });
-            $(".alert-danger b").text("Тут что-то может быть.. иногда.. наверное..");
+            // sort by descending
+            $('#filterBtn')['on']('click', filterByCoins);
+            $('#resetFilter')['on']('click', resetFilter);
 
+            // sort by price
+            timetoreload();
         });
 
     }
 
-    hello();
+    htmlwriter();
+    styles();
     DoAfter();
 
 })();
